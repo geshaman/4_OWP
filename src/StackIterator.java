61cbb89ebbee99c8be24a9ed198463523587fa07
geshaman <glebovyaroslav58@gmail.com>
2025-11-20 12:19:22 +0300

    Добавил метод, инициализирующий итератор

--- src/StackIterator.java
+++ src/StackIterator.java
@@ -9,15 +9,29 @@ public class StackIterator<T> extends Stack<T> {
     /** Поле, хранящее объект-итератор для обхода по коллекции*/
     private Iterator<T> _iterator;
 
-    /** Возвращает следующий элемент в итерации */
+    /**
+     * Инициализирует итератор
+     */
+    public void initIterator() {
+        this._iterator = super.iterator();
+    }
+
+    /**
+     * Возвращает следующий элемент в итерации
+     * Переопределяет стандартный pop() для работы через итератор
+     */
     @Override
     public T pop() {
         return _iterator.next();
     }
 
-    /** Проверяет, есть ли еще элементы для итерации */
+    /**
+     * Проверяет, есть ли еще элементы для итерации
+     * Переопределяет стандартный empty() для работы через итератор
+     */
     @Override
     public boolean empty() {
         return !_iterator.hasNext();
     }
+
 }
